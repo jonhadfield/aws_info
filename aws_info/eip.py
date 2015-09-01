618c877c4325f410f7f348972d6ed002c124232e
Jon Hadfield <jon@lessknown.co.uk>
2015-09-01 15:57:25 +0100

    remove comments.

--- aws_info/eip.py
+++ aws_info/eip.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python
-#import boto.ec2
-#import boto.ec2.image
-#import boto.exception
-#import os
-#import re
-#import sys
 
 def get_unassociated_eips(connection):
     all_ips = connection.get_all_addresses()
